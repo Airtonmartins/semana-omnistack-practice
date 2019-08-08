0808d98b2614ddffd44b96ce31babfea4357a950
Airton Neris <airtonmartinsneris@hotmail.com>
2019-08-08 00:24:24 -0300

    DislikeController and route /devs/:devId/dislikes created

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const DevController = require('./controller/DevController');
 const LikeController = require('./controller/LikeController');
+const DislikeController = require('./controller/DislikeController');
 
 const routes = express.Router();
-
-
 
 routes.get('/', (req, res) => {
     return res.json({ message: `Hello ${req.query.name}` });
@@ -12,5 +11,6 @@ routes.get('/', (req, res) => {
 
 routes.post('/devs', DevController.store);
 routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
 module.exports = routes;
