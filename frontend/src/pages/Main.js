008b6d314b48ed506023e2767e3cdcba2319106b
Carvalho, Taiane V <taiane.v.carvalho@accenture.com>
2019-08-09 02:34:56 -0300

    Main page integrated with backend

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,79 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Main.css';
+
+import api from '../services/api';
 
 import logo from '../assets/tindev.svg'
 import dislike from '../assets/dislike.svg'
 import like from '../assets/like.svg'
 
 export default function Main({ match }) {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        async function loadUsers(){
+            const response = await api.get('/devs',{
+                headers: {
+                    user: match.params.id,
+                }
+            })
+            setUsers(response.data);
+        }
+        loadUsers();
+    }, [match.params.id]);
+
+
     return (
-        <div className='main-container'>
+        <div className="main-container">
             <img src={logo} alt="Tindev" />
             <ul>
-                <li>
-                    <img src="https://avatars2.githubusercontent.com/u/2254731?v=4" alt="" />
-                    <footer>
-                        <strong>Diego</strong>
-                        <p>rem ipsum dolor sit amet, consectetur adipiscing elit</p>
-                    </footer>
+                {users.map(user => (
+                    <li key={user._id}>
+                        <img src={user.avatar} alt={user.avatar} />
+                        <footer>
+                            <strong>{user.name}</strong>
+                            <p>{user.bio}</p>
+                        </footer>
 
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="Like" />
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://avatars2.githubusercontent.com/u/2254731?v=4" alt="" />
-                    <footer>
-                        <strong>Diego</strong>
-                        <p>rem ipsum dolor sit amet, consectetur adipiscing elit</p>
-                    </footer>
-
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="Like" />
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://avatars2.githubusercontent.com/u/2254731?v=4" alt="" />
-                    <footer>
-                        <strong>Diego</strong>
-                        <p>rem ipsum dolor sit amet, consectetur adipiscing elit</p>
-                    </footer>
-
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="Like" />
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://avatars2.githubusercontent.com/u/2254731?v=4" alt="" />
-                    <footer>
-                        <strong>Diego</strong>
-                        <p>rem ipsum dolor sit amet, consectetur adipiscing elit</p>
-                    </footer>
-
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="Like" />
-                        </button>
-                    </div>
-                </li>
+                        <div className="buttons">
+                            <button type="button">
+                                <img src={dislike} alt="Dislike" />
+                            </button>
+                            <button type="button">
+                                <img src={like} alt="Like" />
+                            </button>
+                        </div>
+                    </li>
+                ))}
+                
             </ul>
         </div>
     );
