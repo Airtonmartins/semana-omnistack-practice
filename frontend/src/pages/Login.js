70d971eaf722c715268aa50293a4ab001ae0bab6
Carvalho, Taiane V <taiane.v.carvalho@accenture.com>
2019-08-08 23:34:53 -0300

    page Login style added

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './Login.css';
 
 import logo from '../assets/tindev.svg'
 
 export default function Login () {
     return (
-        <img src={logo} alt="Tindev" />
+        <div className='login-container'>
+            <form>
+                <img src={logo} alt="Tindev" />
+                <input placeholder="Digite seu usuário do Github" />
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
     );
 }
