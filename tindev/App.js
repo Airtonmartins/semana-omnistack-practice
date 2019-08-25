e1c396ac2ec6dd26ea9ff6e9f0ae73d3505647f4
Airton Neris <airtonmartinsneris@hotmail.com>
2019-08-25 19:43:47 -0300

    hello world incremented

--- tindev/App.js
+++ tindev/App.js
@@ -7,12 +7,27 @@
  */
 
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-const App = () => {
+export default function App () {
   return (
-    <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
+    </View>
     );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    backgroundColor: '#7159c1', 
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  text: {
+    fontWeight: 'bold',
+    color: '#FFF',
+    fontSize: 20
+  },
+});
