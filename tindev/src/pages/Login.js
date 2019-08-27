d88aa632e5473025198f33cdd57dbea10df72c48
Airton Neris <airtonmartinsneris@hotmail.com>
2019-08-26 22:22:51 -0300

    login page integrated with backend

--- tindev/src/pages/Login.js
+++ tindev/src/pages/Login.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage'
 import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.png';
 
 export default function Login({ navigation }) {
-    function handleLogin() {
+    const [user, setUser] = useState('');
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user =>{
+            if (user) {
+                navigation.navigate('Main', { user })
+            }
+        })
+    }, []);
+
+    async function handleLogin() {
+        const response = await api.post('/devs', { username: user });
+
+        const { _id } = response.data;
+
+        await AsyncStorage.setItem('user', _id)
+
         navigation.navigate('Main')
     }
 
@@ -17,7 +36,10 @@ export default function Login({ navigation }) {
                 autoCorrect={false}
                 placeholder="Digite seu usuário do Github" 
                 placeholderTextColor="#999"
-                style={styles.input}/>
+                style={styles.input}
+                value={user}
+                onChangeText={setUser}
+                />
             
             <TouchableOpacity onPress={handleLogin} style={styles.button}>
                 <Text style={styles.buttonText}>Enviar</Text>
