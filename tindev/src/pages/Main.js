77d7323ac32ef1e2018485e64f7c3c8054483f5b
Airton Neris <airtonmartinsneris@hotmail.com>
2019-08-27 00:36:57 -0300

    main page integreted with backend

--- tindev/src/pages/Main.js
+++ tindev/src/pages/Main.js
@@ -1,37 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, SafeAreaView, Image, StyleSheet, TouchableOpacity } from 'react-native';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.png';
 import like from '../assets/like.png';
 import dislike from '../assets/dislike.png';
 
-export default function Main() {
+export default function Main({ navigation }) {
+    const id = navigation.getParam('user');
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function loadUsers(){
+            const response = await api.get('/devs',{
+                headers: {
+                    user: id,
+                }
+            })
+            setUsers(response.data);
+        }
+        loadUsers();
+    }, [id]);
+
+    async function handlerLike(id){
+        await api.post(`/devs/${id}/likes`, null, {
+            headers: { user: id },
+        })
+
+        setUsers(users.filter(user => user._id != id));
+    }
+
+    async function handleDislike(id){
+        await api.post(`/devs/${id}/dislikes`, null, {
+            headers: { user: id },
+        })
+
+        setUsers(users.filter(user => user._id != id));
+
+    }
+
+
     return (
     <SafeAreaView style={styles.container}>
         <Image style={styles.logo} source={logo} />
 
         <View style={styles.cardsContainer}>
-            <View style={[styles.card, { zIndex: 3 }]}>
-                <Image style={styles.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/2254731' }} />
-                <View style={styles.footer}>
-                    <Text style={styles.name}>Diego Fernandes</Text>
-                    <Text style={styles.bio} numberOfLines={3}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque venenatis sagittis tellus, et feugiat lorem scelerisque sit amet. Morbi mattis ex nec tellus auctor rhoncus. Nulla quis rutrum sapien. I</Text>
-                </View>
-            </View>
-            <View style={[styles.card, { zIndex: 2 }]}>
-                <Image style={styles.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/2254731' }} />
-                <View style={styles.footer}>
-                    <Text style={styles.name}>Diego Fernandes</Text>
-                    <Text style={styles.bio} numberOfLines={3}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque venenatis sagittis tellus, et feugiat lorem scelerisque sit amet. Morbi mattis ex nec tellus auctor rhoncus. Nulla quis rutrum sapien. I</Text>
-                </View>
-            </View>
-            <View style={[styles.card, { zIndex: 1 }]}>
-                <Image style={styles.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/2254731' }} />
-                <View style={styles.footer}>
-                    <Text style={styles.name}>Diego Fernandes</Text>
-                    <Text style={styles.bio} numberOfLines={3}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque venenatis sagittis tellus, et feugiat lorem scelerisque sit amet. Morbi mattis ex nec tellus auctor rhoncus. Nulla quis rutrum sapien. I</Text>
-                </View>
-            </View>
+            { users.length === 0 
+                ? <Text style={styles.empty}>Acabou :(</Text> 
+                : (
+                    users.map((user, index) => (
+                        <View style={[styles.card, { zIndex: users.length - index }]}>
+                            <Image style={styles.avatar} source={{ uri: user.avatar }} />
+                            <View style={styles.footer}>
+                                <Text style={styles.name}>{user.name}</Text>
+                                <Text style={styles.bio} numberOfLines={3}>{user.bio}</Text>
+                            </View>
+                        </View>
+                    ))
+                )
+            }
         </View>
         <View style={styles.buttonsContainer}>
             <TouchableOpacity style={styles.button}>
@@ -117,6 +145,13 @@ const styles = StyleSheet.create({
         marginHorizontal: 20, 
         elevation: 2,
 
+    },
+
+    empty: {
+        alignSelf: 'center',
+        color: '#999',
+        fontSize: 24,
+        fontWeight: 'bold'
     },
 
 });
