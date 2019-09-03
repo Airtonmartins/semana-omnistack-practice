36a93c7fa2274f8442e7160f5082907f99f71ff6
Airton Neris <airtonmartinsneris@hotmail.com>
2019-09-02 22:36:40 -0300

    hello world web socket

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,15 +4,31 @@ const cors = require('cors');
 
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    console.log('Nova conexão', socket.id);
+
+    socket.on('hello', message => {
+        console.log(message);
+    })
+
+    setTimeout(() =>{
+        socket.emit('world', {
+            message: 'OmnStack'
+        });
+    }, 5000)
+});
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-rd4fb.mongodb.net/omnistack8?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
 
