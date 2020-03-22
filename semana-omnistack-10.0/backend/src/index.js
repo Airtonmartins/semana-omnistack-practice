e326d93122a76e751ce2b17872d5acbcfaad4a6c
Airton Martins <airtonmartinsneris@hotmail.com>
2020-03-22 19:04:02 -0300

    [semana-omnistack-10.0] Cria endpoint de cadastro de Devs
    * Inserindo container do mongoDB no docker-compose e linkando ao backend.
    
    * Inserido o mongoose nas configurações do projeto para conectar-se com o MongoDB
    
    * Criação do DevSchema como objeto que irá persistir os dados no banco de dados.
    
    * Criação do endpoint POST /devs para salvar um Dev recebendo o username do Github e suas stacks.

---
+++ semana-omnistack-10.0/backend/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect(
+    'mongodb://mongo/express-mongo', 
+    { useNewUrlParser: true }
+    ).then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+app.use(express.json());    
+app.use(routes);
+
+app.listen(3333);
+
