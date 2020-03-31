73c089a1ba6e3776d4604146483790e024717d53
Airtonmartins <airtonmartinsneris@hotmail.com>
2020-03-30 23:10:42 -0300

    [semana-omnstack-10.0]  Preenche informações de geolocalização no formulário
    * Inserido código que pega informações de geolocalização do navegador para preencher os campos do formulário.
    
    * inserido os "states" de cada campo do formulário

--- semana-omnistack-10.0/web/src/App.js
+++ semana-omnistack-10.0/web/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './global.css'
 import './App.css'
@@ -6,7 +6,28 @@ import './Sidebar.css'
 import './Main.css'
 
 function App() {
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    )
+  }, []);
+  
   return (
     <div id="app">
       <aside>
@@ -14,23 +35,49 @@ function App() {
         <form>
           <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
+            <input 
+              name="github_username" 
+              id="github_username" 
+              required
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)}
+            />
           </div>
 
           <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
+            <input 
+              name="techs" 
+              id="techs" 
+              required 
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+            />
           </div>
 
           <div className="input-group">
             <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
+              <input 
+                name="latitude"
+                type="number"
+                id="latitude" 
+                required 
+                value={latitude} 
+                onChange={e => setLatitude(e.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
+              <input 
+                name="longitude" 
+                type="number" 
+                id="longitude" 
+                required 
+                value={longitude} 
+                onChange={e => setLongitude(e.target.value)}
+              />
             </div>
           </div>
 
@@ -51,7 +98,7 @@ function App() {
             <a href="https://github.com/Airtonmartins">Acessar perfil no GitHub</a> 
           </li>
           <li className="dev-item">
-            <header>
+            <header >
               <img src="https://avatars0.githubusercontent.com/u/15370827?s=460&u=72c8eff344676cd2b045e57675028a20b46b8a03&v=4" alt="Airtonmartins"></img>
               <div className="user-info">
                 <strong>Airtonmartins</strong>
