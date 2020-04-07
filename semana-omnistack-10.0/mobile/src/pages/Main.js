cea9c4db86ac194331a23450009275afa69393fd
Airtonmartins <airtonmartinsneris@hotmail.com>
2020-04-06 23:01:31 -0300

    [semana-omnstack-10.0] Insere barra de pesquisa
    * Barra de pesquisa por usuário inserida e estilizada.

--- semana-omnistack-10.0/mobile/src/pages/Main.js
+++ semana-omnistack-10.0/mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity, Keyboard } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
@@ -36,20 +37,35 @@ function Main({ navigation }) {
     }
 
     return (
-        <MapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={{ latitude: -8.0853931, longitude: -34.8906586 }}>
-                <Image style={styles.avatar} source={{ uri : 'https://avatars0.githubusercontent.com/u/15370827?s=460&u=72c8eff344676cd2b045e57675028a20b46b8a03&v=4' }} />
-                <Callout onPress={() => {
-                    navigation.navigate('Profile', { github_username: 'Airtonmartins'});
-                }}>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Airtonmartins</Text>
-                        <Text style={styles.devBio}>Pythonista, Javista e vulgo PaiDocker, entusiasta da cultura Devops.</Text>
-                        <Text style={styles.devTechs}>Python, Docker, Java</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+                <Marker coordinate={{ latitude: -8.0853931, longitude: -34.8906586 }}>
+                    <Image style={styles.avatar} source={{ uri : 'https://avatars0.githubusercontent.com/u/15370827?s=460&u=72c8eff344676cd2b045e57675028a20b46b8a03&v=4' }} />
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', { github_username: 'Airtonmartins'});
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Airtonmartins</Text>
+                            <Text style={styles.devBio}>Pythonista, Javista e vulgo PaiDocker, entusiasta da cultura Devops.</Text>
+                            <Text style={styles.devTechs}>Python, Docker, Java</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+            <View style={styles.searchForm}>
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder="Buscar devs por techs..."
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+
+                <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                    <MaterialIcons name="my-location" size={20} color="#FFF" />
+                </TouchableOpacity>
+            </View>
+        </>
     );
 }
 
@@ -82,8 +98,43 @@ const styles = StyleSheet.create({
 
     devTechs: {
         marginTop: 5,
-    }
+    },
 
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 2,
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8E4Dff',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15,
+    },
 })
 
 export default Main;
