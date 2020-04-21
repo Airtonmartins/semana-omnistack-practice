39a8595bf9d978a013827be46b98dcde64681658
Airtonmartins <airtonmartinsneris@hotmail.com>
2020-04-21 19:43:38 -0300

    [semana-omnstack-10.0] Cria server e client web socket
    * Criando base para utilizar conexões via websocket entre o server backend e a aplicação mobile.

--- semana-omnistack-10.0/backend/src/index.js
+++ semana-omnistack-10.0/backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
+
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect(
     'mongodb://mongo/express-mongo', 
@@ -15,5 +21,5 @@ app.use(cors())
 app.use(express.json());    
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
 
