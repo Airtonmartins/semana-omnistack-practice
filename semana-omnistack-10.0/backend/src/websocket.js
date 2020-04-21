a5cd510faf91313a32affe7b59cc52e88eebb547
Airtonmartins <airtonmartinsneris@hotmail.com>
2020-04-21 20:32:36 -0300

    [semana-omnstack-10.0] Cria filtro de conexões
    * Criação de mecanismo que filtra conexões de acordo com tecnologias e localizações assim que um novo dev é criado, feito isso para posteiormente enviar o novo dev para as conexões.

--- semana-omnistack-10.0/backend/src/websocket.js
+++ semana-omnistack-10.0/backend/src/websocket.js
@@ -1,10 +1,29 @@
 const socketio = require('socket.io');
+const parseStringsAsArray = require('./utils/parseStringAsArray');
+const calculateDistance = require('./utils/calculateDistance');
+
+const connections = [];
 
 exports.setupWebsocket = (server) => {
     const io = socketio(server);
 
     io.on('connection', socket => {
-        console.log(socket.id);
-        console.log(socket.handshake.query);
+        const { latitude, longitude, techs  } = socket.handshake.query;
+
+        connections.push({
+            id: socket.id,
+            coordinates: {
+                latitude: Number(latitude),
+                longitude: Number(longitude),
+            },
+            techs: parseStringsAsArray(techs),
+        });
     });
 };
+
+exports.findConnections = (coordinates, techs) => {
+    return connections.filter(connection => {
+        return calculateDistance(coordinates, connection.coordinates) < 10
+            && connection.techs.some(item => techs.includes(item))
+    })
+}
