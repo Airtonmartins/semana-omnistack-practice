ae5634e6887d01bcdbae130e36841b7e8b08ae75
Airtonmartins <airtonmartinsneris@hotmail.com>
2020-04-22 20:58:03 -0300

    [semana-omnstack-10.0] Envia novo deve pelo websocket
    * Enviando uma mensagem no canal de websocket new-dev, com as informações de um novo dev cadastrado para usuários que estão a um raio de 10km e que procuram habilidades que o novo dev tenha.

--- semana-omnistack-10.0/mobile/src/services/socket.js
+++ semana-omnistack-10.0/mobile/src/services/socket.js
@@ -4,6 +4,10 @@ const socket = socketio('http://192.168.25.237:3333', {
     autoConnect: false,
 });
 
+function subscribeToNewDevs(subcribeFunction) {
+    socket.on('new-dev', subcribeFunction);
+}
+
 function connect(latitude, longitude, techs) {
     socket.io.opts.query = {
         latitude,
@@ -23,4 +27,5 @@ function disconnect() {
 export {
     connect,
     disconnect,
+    subscribeToNewDevs,
 };
